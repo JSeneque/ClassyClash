514ca477be19acb7db0ce13f6aa291683bb839f3
Jean-Noel <jeannoelseneque@hotmail.com>
2021-08-05 14:05:13 +0930

    Add movement around the map

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include "raylib.h"
+#include "raymath.h"
 int main()
 {
     const int windowWidth {640};
     const int windowHeight {380};
 
     InitWindow(windowWidth, windowHeight, "Classy Clash");
+
     Texture2D map = LoadTexture("./Tileset/map.png");
+    Vector2 mapPos{0.0, 0.0};
+
+    const float speed {4.0f};
 
     SetTargetFPS(60);
 
@@ -13,7 +18,19 @@ int main()
     {
         BeginDrawing();
         ClearBackground(WHITE);
-        Vector2 mapPos{0.0, 0.0};
+
+        Vector2 direction {};
+
+        if (IsKeyDown(KEY_A)) direction.x -= 1.0;
+        if (IsKeyDown(KEY_D)) direction.x += 1.0;
+        if (IsKeyDown(KEY_W)) direction.y -= 1.0;
+        if (IsKeyDown(KEY_S)) direction.y += 1.0;
+        if (Vector2Length(direction) != 0.0)
+        {
+            mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+
+        }
+        
         DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 
         EndDrawing();
